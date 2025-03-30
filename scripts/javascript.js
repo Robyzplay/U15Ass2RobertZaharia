933c19d138330e3b9f30e795664936cb14d5686a
Rob <robertzaharia3568@gmail.com>
2025-03-30 23:54:42 +0100

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -124,13 +124,6 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
 
-    menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
-});
 
 
